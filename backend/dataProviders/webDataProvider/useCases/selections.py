7b7e55a86a3871eecec5d6f188cf4a5ab822a229
tom.mansion <tomansion@yahoo.fr>
2023-01-06 18:00:37 +0100

    Fixed black and requierements

--- backend/dataProviders/webDataProvider/useCases/selections.py
+++ backend/dataProviders/webDataProvider/useCases/selections.py
@@ -4,7 +4,7 @@ from utils.utils import timeNow
 
 def get_project_selections(url, project_id):
     selections = api.get_selections(url, project_id)
-    
+
     debiai_selections = []
     for selection in selections:
         debiai_selections.append(
@@ -17,7 +17,6 @@ def get_project_selections(url, project_id):
             }
         )
     return debiai_selections
-    
 
 
 def get_id_list_from_selection(url, project_id, selection_id):
@@ -25,17 +24,14 @@ def get_id_list_from_selection(url, project_id, selection_id):
 
 
 def create_selection(url, project_id, name, id_list, request_id):
-    
-    data = {
-        "idList": id_list,
-        "name": name
-    }
-    
+
+    data = {"idList": id_list, "name": name}
+
     if request_id is not None:
         data["request"]: request_id
-        
+
     return api.post_selection(url, project_id, data)
+
 
 def delete_selection(url, project_id, selection_id):
     return api.delete_selection(url, project_id, selection_id)
-    
