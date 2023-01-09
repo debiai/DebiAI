810b8080c4acec08bb558291356cda929e25c02e
Nicolas Leroux <nicolas@E840-5CG026BQX0.irt-systemx.local>
2023-01-09 17:20:10 +0100

    add try catch on get data

--- backend/controller/data.py
+++ backend/controller/data.py
@@ -2,6 +2,7 @@
 # Imports
 #############################################################################
 import dataProviders.dataProviderManager as data_provider_manager
+import dataProviders.DataProviderException as DataProviderException
 
 #############################################################################
 # Data Management
@@ -14,22 +15,27 @@ def get_data(projectId, data):
     projectId = projectId.split("|")[1]
     sampleIds = data["sampleIds"]
 
+
     # Find the data provider
     data_provider = data_provider_manager.get_single_data_provider(data_provider_id)
+    try:
+            
+        # Ask for the data
+        samples = data_provider.get_samples(projectId, sampleIds)
 
-    # Ask for the data
-    samples = data_provider.get_samples(projectId, sampleIds)
 
-    if samples is not None:
-        return {
-            "data": samples,
-            "dataMap": True,
-        }, 200  # TODO Dict response need to change in frontend
+        if samples is not None:
+            return {
+                "data": samples,
+                "dataMap": True,
+            }, 200
 
-    return (
-        "Can't find samples for project "
-        + projectId
-        + " on data provider : "
-        + data_provider_id,
-        404,
-    )
+        return (
+            "Can't find samples for project "
+            + projectId
+            + " on data provider : "
+            + data_provider_id,
+            404,
+        )
+    except DataProviderException.DataProviderException as e:
+        return e.message, e.status_code
