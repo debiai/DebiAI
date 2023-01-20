f757dee5808eac375e10543c9f67333988b6a55d
tom.mansion <tomansion@yahoo.fr>
2023-01-20 11:53:50 +0100

    Done with backend and routes

---
+++ backend/controller/widgetConfigurations.py
@@ -0,0 +1,23 @@
+#############################################################################
+# Imports
+#############################################################################
+import utils.debiai.widgetConfigurations as widgetConfUtils
+
+#############################################################################
+# Widget configuration Management
+#############################################################################
+
+
+def get_configurations(widgetTitle):
+    configurations = widgetConfUtils.get_configurations(widgetTitle)
+    return configurations, 200
+
+
+def post_configuration(widgetTitle, data):
+    widgetConfUtils.add_configuration(widgetTitle, data)
+    return 204
+
+
+def delete_configuration(widgetTitle, id):
+    widgetConfUtils.delete_configuration(widgetTitle, id)
+    return 204
---
+++ backend/utils/debiai/widgetConfigurations.py
@@ -0,0 +1,102 @@
+import os
+import ujson as json
+import utils.utils as utils
+import uuid
+
+CONF_PATH = "data/widgetConfigurations.json"
+
+# Configuration file structure
+# {
+#     "widgetTitle": [
+#         {
+#             "id": ""
+#             "name": ""
+#             "description": "",
+#             "projectId": ""
+#             "dataProviderId": ""
+#             "creationDate": 0
+#
+#             "configuration": {},
+#         },
+#         ...
+#      ]
+# }
+
+
+def setup_widget_configurations():
+    # Create the folder if it does not exist
+    if not os.path.exists("data"):
+        os.mkdir("data")
+
+    # Create the file if it does not exist
+    if not os.path.exists(CONF_PATH):
+        _save_configurations({})
+
+
+def get_configurations(widget_title):
+    # Return the configurations list of the widget
+    try:
+        with open(CONF_PATH) as json_file:
+            configurations = json.load(json_file)
+            if widget_title in configurations:
+                return configurations[widget_title]
+            else:
+                return {}
+
+    except FileNotFoundError:
+        setup_widget_configurations()
+        return {}
+    except json.decoder.JSONDecodeError as e:
+        print("Error while reading the widget configurations file")
+        print(e)
+        print("The file will be reset")
+        _save_configurations({})
+        return {}
+
+
+def add_configuration(widget_title, data):
+    # project_id, data_provider_id, conf_description, conf_name, conf
+    # Add a new widget configuration
+    configurations = get_configurations()
+
+    if widget_title not in configurations:
+        configurations[widget_title] = []
+
+    # Generate id
+    id = uuid.uuid1()
+
+    configuration_to_add = {
+        "id": id,
+        "name": data["name"],
+        "description": data["description"],
+        "projectId": data["projectId"],
+        "dataProviderId": data["dataProviderId"],
+        "creationDate": utils.timeNow(),
+        "configuration": data["configuration"],
+    }
+
+    # Save configuration
+    configurations[widget_title].append(configuration_to_add)
+    _save_configurations(configurations)
+
+
+def delete_configuration(widget_title, id):
+    # Delete the widget configuration by its name
+    configurations = get_configurations()
+
+    if widget_title in configurations:
+        for configuration in configurations[widget_title]:
+            if configuration["id"] == id:
+                configurations[widget_title].remove(configuration)
+
+        _save_configurations(configurations)
+
+
+def _save_configurations(conf):
+    # Update the json file
+    try:
+        with open(CONF_PATH, "w") as json_file:
+            json.dump(conf, json_file)
+    except FileNotFoundError:
+        setup_widget_configurations()
+        _save_configurations(conf)
