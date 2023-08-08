4b0398d9ffc3c2033eac906028499ec1aba9a1ba
tom.mansion <tomansion@yahoo.fr>
2023-08-08 11:48:36 +0200

    WIP navbar

--- frontend/src/components/debiai/dataAnalysis/SideBar.vue
+++ frontend/src/components/debiai/dataAnalysis/SideBar.vue
@@ -1,17 +1,62 @@
 <template>
-  <div id="SideBar">
+  <div>
+    <!-- Background -->
+    <transition name="fade">
+      <div
+        id="background"
+        v-show="mouseHover"
+      />
+    </transition>
+
+    <!-- SideBar -->
     <div
-      class="section"
-      v-for="section in menuList"
-      :key="section.name"
+      id="SideBar"
+      @mouseover="mouseHover = true"
+      @mouseleave="mouseHover = false"
     >
       <div
-        class="subSection"
-        v-for="subSection in section.menuList"
-        :key="subSection.name"
+        class="section"
+        v-for="section in menuList"
+        :key="section.name"
       >
-        <div class="subSectionName">{{ subSection.name }}</div>
-        <div class="subSectionDescription">{{ subSection.description }}</div>
+        <div class="side">
+          <!-- The visible part when the mouse is not hovering the sidebar -->
+          <div class="svg">
+            <inline-svg
+              :src="require('@/assets/svg/filter.svg')"
+              width="18"
+              height="18"
+              style="margin-right: 3px"
+            />
+          </div>
+
+          <!-- The opening part when the mouse is hovering the sidebar -->
+          <transition name="scale">
+            <div
+              class="name"
+              v-show="mouseHover"
+            >
+              {{ section.name }}
+            </div>
+          </transition>
+        </div>
+
+        <!-- The opening part when the mouse is hovering the sidebar -->
+        <!-- <transition name="fade">
+          <div
+            class="subsections"
+            v-show="mouseHover"
+          >
+            <div
+              class="subSection"
+              v-for="subSection in section.menuList"
+              :key="subSection.name"
+            >
+              <div class="subSectionName">{{ subSection.name }}</div>
+              <div class="subSectionDescription">{{ subSection.description }}</div>
+            </div>
+          </div>
+        </transition> -->
       </div>
     </div>
   </div>
@@ -37,25 +82,65 @@ export default {
       // }
     },
   },
+
+  data() {
+    return {
+      mouseHover: false,
+    };
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-#SideBar {
-  width: 100px;
+#background {
+  width: 100%;
   height: 100%;
   position: fixed;
   top: 62px;
   left: 0px;
-  z-index: 1;
+  z-index: 0;
+
+  background: rgba(0, 0, 0, 0.5);
+  backdrop-filter: blur(1px);
+}
+
+#SideBar {
+  width: 80px;
+  height: 100%;
+  position: fixed;
+  top: 62px;
+  left: 0px;
+  z-index: 0;
 
   background: #e8e8e8;
   border-right: #bab9bf 2px solid;
 
-  transition: width 0.3s;
+  transition: all 0.3s;
+
+  .section {
+    padding: 10px 10px;
+    font-size: 14px;
+
+    .side {
+      display: flex;
+      flex-direction: row;
+      justify-content: flex-start;
+      align-items: center;
+      gap: 10px;
+      height: 40px;
+
+      color: #494949;
+    }
+  }
 
   &:hover {
     width: 400px;
+    box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.5);
+
+    .section {
+      .side {
+      }
+    }
   }
 }
 </style>
