964b932462960eb182fa328aab648bbbd7baba2d
tom.mansion <tomansion@yahoo.fr>
2024-02-19 16:37:17 +0100

    Fixed issue where we display the project id instead of the name

--- frontend/src/components/debiai/project/Project.vue
+++ frontend/src/components/debiai/project/Project.vue
@@ -194,6 +194,7 @@ export default {
           // Store some info
           this.$store.commit("setProjectColumns", this.project.columns);
           this.$store.commit("setProjectResultsColumns", this.project.resultStructure);
+          this.$store.commit("setProjectName", this.project.name);
 
           // Change the browser title
           if (this.project.name) document.title = this.project.name;
