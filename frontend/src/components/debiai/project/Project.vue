5d25b7391b51d90e6622c27c221682fae24e2d31
tom.mansion <tomansion@yahoo.fr>
2024-02-19 16:24:51 +0100

    Fixed project ID in document title

--- frontend/src/components/debiai/project/Project.vue
+++ frontend/src/components/debiai/project/Project.vue
@@ -194,6 +194,10 @@ export default {
           // Store some info
           this.$store.commit("setProjectColumns", this.project.columns);
           this.$store.commit("setProjectResultsColumns", this.project.resultStructure);
+
+          // Change the browser title
+          if (this.project.name) document.title = this.project.name;
+          else document.title = this.project.id;
         })
         .catch((e) => {
           if (e.response && e.response.status === 500) {
