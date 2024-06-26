174631f268ae748d5397814da3eef713a09de464
FadyCoding <fady.bekkar@irt-systemx.fr>
2024-06-26 10:19:35 +0200

    formatting and cspell

--- backend/modules/algoProviders/integratedAlgoProvider/integratedAlgoProvider.py
+++ backend/modules/algoProviders/integratedAlgoProvider/integratedAlgoProvider.py
@@ -29,7 +29,8 @@ def _get_algorithm_python(algorithm_name):
 
     # Import the algorithm
     algorithm_python = __import__(
-        "backend.modules.algoProviders.integratedAlgoProvider.algorithms." + algorithm_file,
+        "backend.modules.algoProviders.integratedAlgoProvider.algorithms."
+        + algorithm_file,
         fromlist=["*"],
     )
 
