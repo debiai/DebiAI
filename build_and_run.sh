e2b08b076036d7066a4ece91c796324818cd5321
FadyCoding <fady.bekkar@irt-systemx.fr>
2024-06-26 11:00:44 +0200

    cspell checks and ignore files added

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-echo "Nettoyage des anciennes constructions..."
 rm -rf build dist backend.egg-info
 
-echo "Construction du package..."
 python3 setup.py sdist bdist_wheel
 
-echo "Installation du package localement..."
 pip install .
 
 debiai-start
