7ad561975cb5ba83c1262dc1f2a41664f8fcc06d
FadyCoding <fady.bekkar@irt-systemx.fr>
2024-07-04 13:20:50 +0200

    new name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="debiai server",
+    name="debiai-gui",
     version="0.1.0",
     packages=find_packages(include=["debiaiServer", "debiaiServer.*"]),
     include_package_data=True,
