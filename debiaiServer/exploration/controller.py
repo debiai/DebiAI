5e8ca8c9b180eca54ddd06b94667fae56004bc36
tom.mansion <tomansion@yahoo.fr>
2025-05-15 15:24:25 +0200

    Exploration API Initiated

---
+++ debiaiServer/exploration/controller.py
@@ -0,0 +1,26 @@
+def get_explorations():
+    return []
+
+
+def create_exploration():
+    pass
+
+
+def get_exploration_available_config():
+    pass
+
+
+def delete_exploration():
+    pass
+
+
+def update_exploration():
+    pass
+
+
+def get_exploration_config():
+    pass
+
+
+def update_exploration_config():
+    pass
