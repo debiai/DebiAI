06c553389cc7f61d28f15e78c6de6c8b0770ed3c
Loic CANTAT <loic@safenai.io>
2025-08-30 13:48:33 +0200

    BREAKING : Front project return object with projects key instead of an array, 304 hash check managed

--- debiaiServer/api/v1/debiai/projects.py
+++ debiaiServer/api/v1/debiai/projects.py
@@ -11,9 +11,6 @@ from debiaiServer.api.v1.exploration_statistics.utils import get_columns_statist
 # PROJECTS Management
 #############################################################################
 
-# TODO LOIC : for API compatibilité, we do not change the registered method content
-# TODO proposer 2 internal module : legacy (v1 implementation & 1 based on parquet)
-
 
 def change_project_overview_v2(project_info, column_info):
     v2_project_info = {
@@ -37,10 +34,30 @@ def change_project_overview_v2(project_info, column_info):
     return v2_project_info
 
 
-def get_projects():
+def freeze(o):
+
+    if isinstance(o, dict):
+        return frozenset({k: freeze(v) for k, v in o.items()}.items())
+
+    if isinstance(o, list):
+        return tuple([freeze(v) for v in o])
+
+    return o
+
+
+def make_hash(o):
+    """
+    makes a hash out of anything that contains only list,dict and hashable types including string and numeric types
+    """
+    return hash(freeze(o))
+
+
+def get_projects(prev_hash_content=None):
+
     # Return a list of project overviews from all the data providers
     data_providers_list = data_provider_manager.get_data_provider_list()
-    projectOverviews = []
+    projectOverviews = {}
+    projectList = []
     for data_provider in data_providers_list:
         try:
             projects = data_provider.get_projects()
@@ -51,7 +68,7 @@ def get_projects():
 
                     stats = get_columns_statistics(data_provider.name,  project["id"])
                     project_inf = change_project_overview_v2(project, stats["columns"])
-                    projectOverviews.append(project_inf)
+                    projectList.append(project_inf)
 
                     if data_provider.name != "Python module Data Provider":
                         project_inf["dataProviderId"] = data_provider.name
@@ -61,4 +78,12 @@ def get_projects():
         except DataProviderException as e:
             print("Warning get DP projects : " + e.message)
 
-    return projectOverviews, 200
+    new_hash = "prj_" + str(+ make_hash(projectList))  # We add a prefix to avoir empty string
+    # TODO : we make the computation and check the hash but a better implementation shall use hash from data_providers
+    print(new_hash, " <=> ", prev_hash_content, type(new_hash), type(prev_hash_content), new_hash == prev_hash_content)
+    if new_hash == prev_hash_content:
+        return None, 304
+    else:
+        projectOverviews["projects"] = projectList
+        projectOverviews["hash_content"] = new_hash
+        return projectOverviews, 200
